121de035c1bf58f5192fbe8ba334073c4c3fe154
Joan Cabezas <joan.santiago.cabezas@gmail.com>
2019-03-30 16:26:14 -0500

    drive.py updated dealing with (200,66,3) shape

--- drive.py
+++ drive.py
@@ -15,6 +15,7 @@ from io import BytesIO
 from keras.models import load_model
 import h5py
 from keras import __version__ as keras_version
+import cv2
 
 sio = socketio.Server()
 app = Flask(__name__)
@@ -61,6 +62,9 @@ def telemetry(sid, data):
         imgString = data["image"]
         image = Image.open(BytesIO(base64.b64decode(imgString)))
         image_array = np.asarray(image)
+
+        image_array = cv2.resize(image_array, (200,160))
+
         steering_angle = float(model.predict(image_array[None, :, :, :], batch_size=1))
 
         throttle = controller.update(float(speed))
